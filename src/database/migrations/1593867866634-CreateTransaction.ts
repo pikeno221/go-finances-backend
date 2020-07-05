c12dc8a6ed5fc04b9e88e8483d1589bd1c79967b
pikeno221 <gabriellgomesbh@gmail.com>
2020-07-05 00:45:26 -0300

    creating migrations

---
+++ src/database/migrations/1593867866634-CreateTransaction.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class CreateTransaction1593867866634 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          }
+        ]
+      })
+    );
+
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+
+}
---
+++ src/database/migrations/1593919390147-CreateCategory.ts
@@ -0,0 +1,44 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { thistle } from "color-name";
+
+export default class CreateCategory1593919390147 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'categories',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
+            },
+            {
+              name: 'title',
+              type: 'varchar',
+              isNullable: false,
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+          },
+          {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+          }
+          ]
+        })
+      )
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('categories');
+
+    }
+
+}
